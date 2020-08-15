ab93a0fc85a8ab0ed0ff30601b1a88e7af3a3465
lucifer-ux <62305405+lucifer-ux@users.noreply.github.com>
2020-08-15 23:53:37 +0530

    Freecodecamp project
    A data visualization project

---
+++ Data_Visualization_Project/index.js
@@ -0,0 +1,122 @@
+let url='https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json';
+let req =new XMLHttpRequest();
+
+let data;
+let values=[];
+let heightScale;
+let xScale;
+let xAxisScale;
+let yAxisScale;
+
+let width=800;
+let height=600;
+let padding=40;
+
+let svg=d3.select('svg');
+
+let drawCanvas=()=>{
+    svg.attr('width',width)
+        .attr('height',height);
+}
+
+let generateScales=()=>{
+heightScale=d3.scaleLinear()
+                .domain([0,d3.max(values,(item)=>{
+                    return item[1]
+                })])
+                .range([0,height-(2*padding)])                
+
+ xScale=d3.scaleLinear()
+            .domain([0,values.length - 1])
+            .range([padding,width - padding])
+
+let datesArray=values.map((item)=>{
+    return new Date(item[0])
+})            
+console.log(datesArray)
+xAxisScale=d3.scaleTime()
+                .domain([d3.min(datesArray),d3.max(datesArray)])
+                .range([padding,width - padding])
+yAxisScale=d3.scaleLinear()
+                .domain([0,d3.max(values,(item)=>{
+                    return item[1]
+                })])
+                .range([height - padding, padding])
+
+}
+
+let toolTip =d3.select('body')
+                .append('div')
+                .attr('id','tooltip')
+                .style('visibility', 'hidden')
+                .style('height', 'auto')
+                .style('width', 'auto')
+
+let drawBars=()=>{
+svg.selectAll('rect')
+    .data(values)
+    .enter()
+    .append('rect')
+    .attr('class','bar')
+    .attr('width',width-(2*padding)/(values.length))
+    .attr('data-date',(item)=>{
+        return item[0]
+    })
+    .attr('data-gdp',(item)=>{
+        return item[1]
+    })
+    .attr('height', (item)=>{
+        return heightScale(item[1])
+    })
+    .attr('x',(item,i)=>{
+        return xScale(i)
+    })
+    .attr('y',(item)=>{
+        return (height - padding)-heightScale(item[1])
+
+    })
+    .on('mouseover',(item)=>{
+        toolTip.transition()
+        .style('visibility','visible')
+        .text(item[0])
+    })
+    .on('mouseout',(item)=>{
+        toolTip.transition()
+        .style('visibility','hidden')
+
+    document.getElementById('tooltip').setAttribute('data-date',(item[0]))    
+    })
+
+}
+
+let generateAxis=()=>{
+    
+let xAxis=d3.axisBottom(xAxisScale)
+let yAxis=d3.axisLeft(yAxisScale)
+svg.append('g')
+    .call(xAxis)
+    .attr('id','x-axis')
+    .attr('transform','translate(0,' + (height- padding) + ')')
+
+svg.append('g')
+    .call(yAxis)
+    .attr('id','y-axis')
+    .attr('transform', 'translate('+(padding)+', 0)')
+}
+
+
+
+req.open('GET',url,true);
+req.onload=()=>{
+data=JSON.parse(req.responseText)
+values=data.data;
+console.log(values);
+drawCanvas();
+generateScales();
+drawBars();
+generateAxis();
+
+}
+
+req.send();
+
